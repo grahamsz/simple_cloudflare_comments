27a85b68d3f561d9dbfacf8677917e8381da058f
Graham Stewart <grahamsz@gmail.com>
2022-12-03 21:18:11 -0700

    fixing license

---
+++ example/functions/scc/_middleware.ts
@@ -0,0 +1,30 @@
+import simpleCloudFlareComments from "pages-plugin-simple-cloudflare-comments";
+import { redirect } from "worker-auth-providers/src/providers/github";
+
+
+
+const hello = async ({ next }) => {
+  const response = await next();
+  if (response.headers.get("location"))
+  {
+    // THis is horribly hacky, but in production we seem to lose our 302 response codes and it breaks everything
+    return new Response(null, {status:302, headers:response.headers});
+  }
+  return response;
+};
+
+
+
+export const onRequest: PagesFunction[] = [
+  
+  hello,
+  simpleCloudFlareComments({
+    googleClientId: "xxxxx",
+    googleClientSecret: "xxxxx",
+
+    authCookieName: "comments_cookie",
+    authCookieSecret: "xxxx_change_this_to_any_random_secret_string",
+    
+
+  })
+];
