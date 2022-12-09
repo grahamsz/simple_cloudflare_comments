286f49612fc29a28dd77ca50b993de1bbd2908b7
Graham Stewart <grahamsz@gmail.com>
2022-12-08 20:53:33 -0700

    fixing package name

--- example/functions/scc/_middleware.ts
+++ example/functions/scc/_middleware.ts
@@ -1,4 +1,4 @@
-import simpleCloudFlareComments from "pages-plugin-simple-cloudflare-comments";
+import simpleCloudFlareComments from "@grahamsz/simple_cloudflare_comments";
 import { redirect } from "worker-auth-providers/src/providers/github";
 
 
